b4d3839f4c5654673e445f597614a89d426ded13
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-20 23:48:27 +0000

    Moved ProfileController to auth namespace

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\ProfileController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
