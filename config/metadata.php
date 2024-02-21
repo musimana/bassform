6af497dde5688cc056c52dcf52cc74a5220f7749
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-21 00:58:05 +0000

    Created app metadata config

---
+++ config/metadata.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Meta Author
+    |--------------------------------------------------------------------------
+    |
+    | The string used for the app's HTML head element's
+    | `<meta name="author">` content.
+    |
+    */
+
+    'author' => env('APP_AUTHOR', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Meta Description
+    |--------------------------------------------------------------------------
+    |
+    | The string used for the app's HTML head element's
+    | `<meta name="description">` content.
+    |
+    */
+
+    'description' => env('APP_DESCRIPTION', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Github Link
+    |--------------------------------------------------------------------------
+    |
+    | Github link for the project's repo. Used in the footer.
+    |
+    */
+
+    'github' => env('APP_LINK_PROJECT_REPO'),
+
+];
