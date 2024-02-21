2e779cdae28011528adb348dece0283f5bfad90e
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-21 18:15:54 +0000

    Replaced Welcome page component with PublicHomepage component

---
+++ resources/js/Pages/Public/PublicHomepage.vue
@@ -0,0 +1,24 @@
+<script setup>
+import PublicLayout from '@/Layouts/PublicLayout.vue';
+import ContentHomepage from '@/Pages/Public/Partials/ContentHomepage.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    content: {
+        type: Object,
+        default: () => ({}),
+    },
+    metadata: {
+        type: Object,
+        default: () => ({}),
+    },
+});
+</script>
+
+<template>
+    <Head :title="metadata?.appName" />
+
+    <PublicLayout>
+        <ContentHomepage />
+    </PublicLayout>
+</template>
