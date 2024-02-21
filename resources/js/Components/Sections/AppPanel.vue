3e5b0f6d29835aaecc8748dc0b4971214c0ec595
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-21 17:17:28 +0000

    Created AppPanel component

---
+++ resources/js/Components/Sections/AppPanel.vue
@@ -0,0 +1,7 @@
+<script setup />
+
+<template>
+    <section class="p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none">
+        <slot />
+    </section>
+</template>
