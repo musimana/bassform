ca0b8eab7e54fda53c52240212a79ef518619931
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-21 02:34:48 +0000

    Updated app.blade head tag metadata elements

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name') }}</title>
+        <meta name="description" content="{{ config('metadata.description') }}" inertia />
+        <meta name="canonical" content="{{ route('home') }}" inertia />
+
+        <base href="{{ url('/') }}">
+        <meta name="robots" content="index, follow" />
+
+        @if(config('metadata.author'))
+        <meta name="author" content="{{ config('metadata.author') }}" />
+        @endif
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -15,6 +25,7 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
+
     <body class="font-sans antialiased">
         @inertia
     </body>
