60faf7b2d5c1580b8fc9b91f1fb592ec7b6a89ec
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-21 17:18:41 +0000

    Created AppMainContent component

---
+++ resources/js/Components/Sections/AppMainContent.vue
@@ -0,0 +1,9 @@
+<script setup />
+
+<template>
+    <main class="max-w-7xl min-h-screen mx-auto p-16 lg:p-32">
+        <div class="mt-16 grid grid-cols-1 gap-6 lg:gap-8">
+            <slot />
+        </div>
+    </main>
+</template>
