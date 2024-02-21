0d9be4495b11bd8ba1d9e9b9881106458f2898d8
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-21 17:27:14 +0000

    Created PublicLayout component

---
+++ resources/js/Layouts/PublicLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import AppNavbar from '@/Components/Navs/Navbars/AppNavbar.vue';
+import AppBody from '@/Components/Sections/AppBody.vue';
+import AppFooter from '@/Components/Sections/AppFooter.vue';
+import AppMainContent from '@/Components/Sections/AppMainContent.vue';
+import AppPanel from '@/Components/Sections/AppPanel.vue';
+</script>
+
+<template>
+    <AppBody>
+        <AppNavbar v-if="$page.props.metadata?.canLogin" />
+
+        <AppMainContent>
+            <AppPanel>
+                <slot />
+            </AppPanel>
+        </AppMainContent>
+
+        <AppFooter />
+    </AppBody>
+</template>
