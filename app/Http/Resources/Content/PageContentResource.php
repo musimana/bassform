387732073d8d966ce0764f736ff1e8b450246224
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-21 01:07:53 +0000

    Created PageContentResource

---
+++ app/Http/Resources/Content/PageContentResource.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Resources\Content;
+
+class PageContentResource
+{
+    /**
+     * Get the resource as an array.
+     *
+     * @return array<string, string>
+     */
+    public function get(): array
+    {
+        return ['bodytext' => ''];
+    }
+}
