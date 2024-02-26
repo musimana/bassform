27b9d55e1cdc1ffed29cfe399ae1ed878169cf2e
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-26 01:49:26 +0000

    Created view resource classes

---
+++ app/Http/Resources/Views/DetailsViewResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Views;
+
+use App\Interfaces\Resources\Items\ArraysToItemInterface;
+
+class DetailsViewResource implements ArraysToItemInterface
+{
+    /**
+     * Get the resource as an array.
+     *
+     * @param  array<string, mixed>  $content  = []
+     * @param  array<string, mixed>  $metadata  = []
+     * @return array<string, mixed>
+     */
+    public function getItem(array $content = [], array $metadata = []): array
+    {
+        return [
+            'content' => $content,
+            'metadata' => (new MetadataViewResource)->getItem($metadata),
+        ];
+    }
+}
---
+++ app/Http/Resources/Views/MetadataViewResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources\Views;
+
+use App\Http\Resources\Views\Navbars\NavbarItemsResource;
+use App\Http\Resources\Views\Public\Formatters\CopyrightMessageResource;
+use App\Interfaces\Resources\Items\ArrayToItemInterface;
+use Illuminate\Support\Facades\Route;
+
+class MetadataViewResource implements ArrayToItemInterface
+{
+    /**
+     * Get a view metadata array for a view by combining the given metadata with the
+     * app's default metadata. Keys in the given metadata array will override keys in
+     * the default metadata array.
+     *
+     * @param  array<string, mixed>  $metadata  = []
+     * @return array<string, mixed>
+     */
+    public function getItem(array $metadata): array
+    {
+        $standard_metadata = [
+            'appName' => config('app.name'),
+            'canLogin' => Route::has('login'),
+            'canonical' => route('home'),
+            'canRegister' => Route::has('register'),
+            'copyright' => (new CopyrightMessageResource)->getValue(),
+            'description' => config('metadata.description'),
+            'links' => [
+                'github' => config('metadata.social_links.github'),
+            ],
+            'navbarItems' => (new NavbarItemsResource)->getItems(),
+            'title' => config('app.name'),
+        ];
+
+        return array_merge($standard_metadata, $metadata);
+    }
+}
