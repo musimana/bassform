b3c91bc99ba31323a14f8f9aedb23c1e3c580cbb
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-26 02:08:43 +0000

    Added traits to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens,
+        HasFactory,
+        HasUuids,
+        Notifiable,
+        SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
