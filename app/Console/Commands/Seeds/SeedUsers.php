5eab9508b0f9bf2b9ea04e628aabeb48553189a6
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-26 01:44:36 +0000

    Created SeedUsers command

---
+++ app/Console/Commands/Seeds/SeedUsers.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands\Seeds;
+
+use App\Console\Commands\AppCommand;
+use App\Models\User;
+use Throwable;
+
+class SeedUsers extends AppCommand
+{
+    private const TABLE_HEADINGS = ['count', 'id', 'email'];
+
+    /** @var array<int, array<int, string>> */
+    private array $table_data = [];
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'seed:users {count=1 : The number of users to seed}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Seed the users table with the specified number of records';
+
+    /** Execute the console command. */
+    public function handle(): int
+    {
+        $this->handleInitialization('Seeding ' . $this->argument('count') . ' user records');
+        $count = strval($this->argument('count'));
+
+        $this->withProgressBar(range(1, $this->argument('count')), function ($i) use ($count) {
+            try {
+                $user = User::factory()->create();
+
+                if ($this->option('verbose')) {
+                    $this->table_data[] = [strval($i) . '/' . $count, $user->id, $user->email];
+                }
+            } catch (Throwable $exception) {
+                return $this->handleError($exception->getMessage());
+            }
+        });
+
+        if ($this->option('verbose')) {
+            $this->newLine();
+            $this->printTable(self::TABLE_HEADINGS, $this->table_data);
+        }
+
+        return $this->handleSuccess('Seeded ' . $count . ' user records, users table now has ' . User::count() . ' records in total');
+    }
+}
