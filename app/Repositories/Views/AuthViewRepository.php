a01227820c4a49081de53d8e93d07187160030fe
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-26 02:03:05 +0000

    Created AuthViewRepository

---
+++ app/Repositories/Views/AuthViewRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories\Views;
+
+use App\Http\Resources\Views\DetailsViewResource;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AuthViewRepository
+{
+    /**
+     * Render the given template after injecting the given data arrays.
+     *
+     * @param  array<string, mixed>  $content  = []
+     * @param  array<string, mixed>  $metadata  = []
+     */
+    public function getViewDetails(string $template, array $content = [], array $metadata = []): Response
+    {
+        return Inertia::render(
+            $template,
+            (new DetailsViewResource)->getItem($content, $metadata)
+        );
+    }
+}
