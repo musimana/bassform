222a8221f8e2a5723970930f794055cebdee491c
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-26 01:37:11 +0000

    Created AppModal component

---
+++ resources/js/Components/Modals/AppModal.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { computed, onMounted, onUnmounted, watch } from 'vue'
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  maxWidth: {
+    type: String,
+    default: '2xl',
+  },
+  closeable: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+const emit = defineEmits(['close'])
+
+watch(
+  () => props.show,
+  () => {
+    if (props.show) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = null
+    }
+  },
+)
+
+const close = () => {
+  if (props.closeable) {
+    emit('close')
+  }
+}
+
+const closeOnEscape = (e) => {
+  if (e.key === 'Escape' && props.show) {
+    close()
+  }
+}
+
+onMounted(() => document.addEventListener('keydown', closeOnEscape))
+
+onUnmounted(() => {
+  document.removeEventListener('keydown', closeOnEscape)
+  document.body.style.overflow = null
+})
+
+const maxWidthClass = computed(() => {
+  return {
+    sm: 'sm:max-w-sm',
+    md: 'sm:max-w-md',
+    lg: 'sm:max-w-lg',
+    xl: 'sm:max-w-xl',
+    '2xl': 'sm:max-w-2xl',
+  }[props.maxWidth]
+})
+
+</script>
+
+<template>
+  <Teleport to="body">
+    <Transition leave-active-class="duration-200">
+      <section
+        v-show="show"
+        class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50 flex"
+        scroll-region
+      >
+        <Transition
+          enter-active-class="ease-out duration-300"
+          enter-from-class="opacity-0"
+          enter-to-class="opacity-100"
+          leave-active-class="ease-in duration-200"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <div v-show="show" class="sm:h-screen fixed inset-0 transform transition-all" @click="close">
+            <div class="absolute inset-0 bg-gray-500/75" />
+          </div>
+        </Transition>
+
+        <Transition
+          enter-active-class="ease-out duration-300"
+          enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+          leave-active-class="ease-in duration-200"
+          leave-from-class="opacity-100 translate-y-0 sm:scale-100"
+          leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div
+            v-show="show"
+            class="bg-white dark:bg-gray-800 dark:bg-gradient-to-bl from-gray-700 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none overflow-hidden transform transition-all sm:w-full m-auto flex"
+            :class="maxWidthClass"
+          >
+            <slot v-if="show" />
+          </div>
+        </Transition>
+      </section>
+    </Transition>
+  </Teleport>
+</template>
