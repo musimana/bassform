82c7d38c77d7c8fc2703468bd07e7f61fdd7607a
Graham Aitken <graham.b.aitken@gmail.com>
2024-02-27 00:47:07 +0000

    Created AppButton component

---
+++ resources/js/Components/Navs/Buttons/AppButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  customClasses: {
+    type: String,
+    default: 'app-button-primary-gray-800',
+  },
+  type: {
+    type: String,
+    default: 'submit',
+  },
+})
+</script>
+
+<template>
+  <button
+    :type="type"
+    class="app-button-base"
+    :class="customClasses"
+  >
+    <slot />
+  </button>
+</template>
