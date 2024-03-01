8c98327dbf27352223a992cca8aff7b18f8f0013
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-01 19:32:54 +0000

    Added ControlDarkMode to NavbarDesktop & NavbarMobile

--- resources/js/Components/Navs/ControlDarkMode.vue
+++ resources/js/Components/Navs/ControlDarkMode.vue
@@ -2,13 +2,20 @@
 import SvgMoon from '@/Components/Images/Svgs/Outline/SvgMoon.vue'
 import SvgSun from '@/Components/Images/Svgs/Outline/SvgSun.vue'
 import { UseDark } from '@vueuse/components'
+
+defineProps({
+  customClasses: {
+    type: String,
+    default: 'w-6 h-6 flex m-auto',
+  },
+})
 </script>
 
 <template>
   <UseDark v-slot="{ isDark, toggleDark }">
-    <span class="w-6 h-6 flex my-auto" @click="toggleDark()">
-      <SvgSun v-if="isDark" class="w-5 h-5 m-auto dark:stroke-yellow-300" />
-      <SvgMoon v-else class="w-4 h-4 m-auto stroke-blue-800" />
+    <span :class="customClasses" @click="toggleDark()">
+      <SvgSun v-if="isDark" class="w-5 h-5 lg:m-auto dark:stroke-yellow-300" />
+      <SvgMoon v-else class="w-4 h-4 lg:m-auto stroke-blue-800" />
     </span>
   </UseDark>
 </template>
