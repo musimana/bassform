6f3c46dbe2bacb806224432cbc8e54c3cc2516b1
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-01 19:01:17 +0000

    Created ControlDarkMode component

---
+++ resources/js/Components/Navs/ControlDarkMode.vue
@@ -0,0 +1,14 @@
+<script setup>
+import SvgMoon from '@/Components/Images/Svgs/Outline/SvgMoon.vue'
+import SvgSun from '@/Components/Images/Svgs/Outline/SvgSun.vue'
+import { UseDark } from '@vueuse/components'
+</script>
+
+<template>
+  <UseDark v-slot="{ isDark, toggleDark }">
+    <span class="w-6 h-6 flex my-auto" @click="toggleDark()">
+      <SvgSun v-if="isDark" class="w-5 h-5 m-auto dark:stroke-yellow-300" />
+      <SvgMoon v-else class="w-4 h-4 m-auto stroke-blue-800" />
+    </span>
+  </UseDark>
+</template>
