90835d39fa9323026d5924c3d25b7ebeb42b7700
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-02 17:46:59 +0000

    Updated DropdownLink styling

--- resources/js/Components/Navs/Dropdowns/DropdownLink.vue
+++ resources/js/Components/Navs/Dropdowns/DropdownLink.vue
@@ -12,7 +12,7 @@ defineProps({
 <template>
   <Link
     :href="href"
-    class="block w-full px-4 py-2 text-sm font-semibold leading-5 text-gray-600 dark:text-gray-100 hover:text-gray-900 dark:hover:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 transition duration-150 ease-in-out"
+    class="block w-full px-4 py-2 text-sm font-semibold leading-5 text-gray-600 dark:text-gray-100 hover:text-gray-900 dark:hover:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 transition duration-150 ease-in-out"
   >
     <slot />
   </Link>
