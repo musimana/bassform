2d8e0f828fa4716c28a76fd89a46757e98738fd3
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-02 18:38:59 +0000

    Updated mobile navbar styling

--- resources/js/Components/Navs/Links/LinkMobile.vue
+++ resources/js/Components/Navs/Links/LinkMobile.vue
@@ -12,10 +12,12 @@ const props = defineProps({
   },
 })
 
+const baseClasses = 'block w-full ps-3 pe-4 py-2 border-l-4 text-start text-base font-semibold focus:outline focus:outline focus:rounded-sm focus:outline-gray-900 dark:focus:outline-gray-100 transition duration-150 ease-in-out'
+
 const classes = computed(() =>
   props.active
-    ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-grey-900 dark:border-gray-100 text-start text-base font-semibold text-gray-900 dark:text-gray-100 bg-gray-200 dark:bg-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-100'
-    : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-semibold text-gray-600 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-200 hover:border-gray-600 dark:hover:border-gray-200 focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-100 transition duration-150 ease-in-out')
+    ? baseClasses + ' border-gray-900 dark:border-gray-100 text-gray-900 dark:text-gray-100 bg-gray-300 dark:bg-gray-900'
+    : baseClasses + ' border-transparent text-gray-600 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-200 hover:border-gray-600 dark:hover:border-gray-200')
 
 </script>
 
