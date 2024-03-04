351ce1db66ca41623087acfb8268eece901d27a0
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-04 02:27:30 +0000

    Created Navbar and NavbarItem models

---
+++ app/Models/Navbar.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Navbar extends Model
+{
+    use HasFactory,
+        SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+    ];
+
+    /** Get the subtitle for the page. */
+    public function getTitle(): string
+    {
+        return $this->title ?? '';
+    }
+
+    /** The relationship for the navbar's items. */
+    public function items(): HasMany
+    {
+        return $this->hasMany(NavbarItem::class)
+            ->whereNull('parent_id')
+            ->orderBy('navbar_items.display_order');
+    }
+}
---
+++ app/Models/NavbarItem.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class NavbarItem extends Model
+{
+    use HasFactory,
+        SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'navbar_id',
+        'parent_id',
+        'title',
+        'url',
+        'display_order',
+    ];
+
+    /** Get the title for the navbar item. */
+    public function getTitle(): string
+    {
+        return $this->title ?? '';
+    }
+
+    /** Get the url for the navbar item. */
+    public function getUrl(): string
+    {
+        return $this->url ?? '#';
+    }
+
+    /** The relationship for the navbar item's children. */
+    public function children(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+}
