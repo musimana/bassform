9d43caff30f64f20e244e0b074da5845e24b74db
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-05 23:26:22 +0000

    Created PublicContentForms component

---
+++ resources/js/Pages/Public/PublicContentForms.vue
@@ -0,0 +1,21 @@
+<script setup>
+import LayoutDefault from '@/Layouts/LayoutDefault.vue'
+import ContentForms from '@/Pages/Public/Partials/ContentForms.vue'
+
+defineProps({
+  content: {
+    type: Object,
+    required: true,
+  },
+  metadata: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <LayoutDefault>
+    <ContentForms />
+  </LayoutDefault>
+</template>
