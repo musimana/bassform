73b56a05d760c2167e1016e0113edb1c086c9f46
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-05 23:22:32 +0000

    Created AppForm component

---
+++ resources/js/Components/Forms/AppForm.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3'
+
+const props = defineProps({
+  endpoint: String,
+  form: Object,
+})
+
+const form = useForm(props.form)
+
+const submit = () => {
+  form.post(props.endpoint, {
+    preserveScroll: true,
+    onSuccess: () => form.reset(),
+  })
+}
+
+</script>
+
+<template>
+  <form @submit.prevent="submit">
+    <slot />
+  </form>
+</template>
