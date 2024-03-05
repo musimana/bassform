d7183c7145d8b2eb2d0194e03106e707aaaa8493
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-05 19:17:00 +0000

    Created ControlsSectionAccordions component

---
+++ resources/js/Pages/Public/Partials/Controls/ControlsSectionAccordions.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppAccordion from '@/Components/Navs/Accordions/AppAccordion.vue'
+</script>
+
+<template>
+  <section id="template-ui-accordions" class="my-8">
+    <h3 class="w-full pb-4 font-semibold text-xs text-gray-950 dark:text-gray-100 uppercase tracking-widest">Accordion Component</h3>
+
+    <AppAccordion>
+      <p class="mb-16">Click to expand this content section.</p>
+
+      <p class="mb-4">Click again to shrink.</p>
+    </AppAccordion>
+  </section>
+</template>
