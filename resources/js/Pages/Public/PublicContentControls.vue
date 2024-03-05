17e8186f40c314ad8681ae17303ff97615402561
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-05 19:23:39 +0000

    Created PublicContentControls page component

---
+++ resources/js/Pages/Public/PublicContentControls.vue
@@ -0,0 +1,21 @@
+<script setup>
+import LayoutDefault from '@/Layouts/LayoutDefault.vue'
+import ContentControls from '@/Pages/Public/Partials/ContentControls.vue'
+
+defineProps({
+  content: {
+    type: Object,
+    required: true,
+  },
+  metadata: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <LayoutDefault>
+    <ContentControls />
+  </LayoutDefault>
+</template>
