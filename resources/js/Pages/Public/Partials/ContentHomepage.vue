37a62772938dd0a263c091deccd03b70e897c8d8
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-06 22:21:49 +0000

    Refactored ContentHomepage component

--- resources/js/Pages/Public/Partials/ContentHomepage.vue
+++ resources/js/Pages/Public/Partials/ContentHomepage.vue
@@ -1,76 +1,30 @@
 <script setup>
-import InputError from '@/Components/Forms/Inputs/InputError.vue'
-import InputFile from '@/Components/Forms/Inputs/InputFile.vue'
-import InputLabel from '@/Components/Forms/Inputs/InputLabel.vue'
-import InputSuccess from '@/Components/Forms/Inputs/InputSuccess.vue'
-import AppButton from '@/Components/Navs/Buttons/AppButton.vue'
-import { useForm, usePage } from '@inertiajs/vue3'
-
-const form = useForm({
-  pdfUpload: {},
-})
-
-const submit = () => {
-  form.post(route('textract.store'), {
-    preserveScroll: true,
-    onSuccess: () => form.reset(),
-  })
-}
-
+import AppPanelLink from '@/Components/Sections/AppPanelLink.vue'
 </script>
 
 <template>
-  <h1 class="text-center text-xl font-bold text-gray-600 dark:text-gray-300">{{ $page.props.metadata.appName }}</h1>
+  <h2 class="text-center text-xl font-bold text-gray-800 dark:text-gray-300">{{ $page.props.metadata.appName }}</h2>
 
-  <hr class="my-8" />
+  <div class="w-full py-8">
+    <hr class="max-w-7xl mx-auto border-gray-700 dark:border-gray-300" />
+  </div>
 
   <article
     v-html="$page.props.content.bodytext"
-    class="mb-8 text-md text-gray-600 dark:text-gray-400"
+    class="text-gray-600 dark:text-gray-300"
   ></article>
 
-  <form @submit.prevent="submit" class="w-full">
-    <div class="w-full flex p-4 bg-gray-300 dark:bg-gray-700">
-      <InputLabel class="my-auto w-full md:w-1/5 text-gray-600 dark:text-gray-400" for="pdfUpload" value="Select Files:" />
-
-      <InputFile
-        class="w-full md:w-4/5 border-gray-300 focus:border-blue-200 focus:ring-blue-200 rounded-md shadow-sm"
-        input-id="pdfUpload"
-        v-model="form.pdfUpload"
+  <div class="w-full mb-8">
+    <div
+      v-if="$page.props.content?.items.length"
+      class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8"
+    >
+      <AppPanelLink
+        v-for="item,index in $page.props.content?.items"
+        :key="index"
+        :item="item"
       />
     </div>
-
-    <div class="w-full flex pt-4 justify-between">
-      <span class="flex mt-4">
-        <InputError class="my-auto" :message="form.errors.pdfUpload" />
-        <InputSuccess class="my-auto" :message="usePage().props.flash.status" />
-      </span>
-
-      <span class="w-1/2 h-8 mt-4 py-1">
-        <progress
-          v-if="form.progress"
-          class="w-full my-auto"
-          max="100"
-          :value="form.progress.percentage"
-        >
-          {{ form.progress.percentage }}%
-        </progress>
-      </span>
-
-      <AppButton
-        custom-classes="app-button-primary-gray-900 mt-4"
-        :class="{ 'opacity-25': form.processing }"
-        :disabled="form.processing"
-      >Upload</AppButton>
-    </div>
-  </form>
-
-  <hr class="my-8" />
-
-  <h2 class="text-lg font-bold text-gray-600 dark:text-gray-300">Results:</h2>
-
-  <div class="w-full flex mt-8 p-4 bg-gray-300 dark:bg-gray-700">
-    <code v-if="usePage().props.flash.output" class="text-gray-600 dark:text-gray-400">{{ usePage().props.flash.output }}</code>
-    <p v-else class="text-gray-600 dark:text-gray-400">[ <em>The response from the form will appear here.</em> ]</p>
+    <em v-else class="block text-center text-gray-600 dark:text-gray-400">No results found.</em>
   </div>
 </template>
