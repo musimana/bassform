f475480658395afe847682869b5831e07c9fc5ef
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-06 23:17:07 +0000

    Renamed OutlineUser component

--- resources/js/Components/Navs/Navbars/NavbarDesktopLogin.vue
+++ resources/js/Components/Navs/Navbars/NavbarDesktopLogin.vue
@@ -1,7 +1,7 @@
 <script setup>
 import DropdownHover from '@/Components/Navs/Dropdowns/DropdownHover.vue'
 import DropdownLink from '@/Components/Navs/Dropdowns/DropdownLink.vue'
-import SvgUser from '@/Components/Images/Svgs/Outline/SvgUser.vue'
+import OutlineUser from '@/Components/Icons/HeroIcons/Outline/OutlineUser.vue'
 </script>
 
 <template>
@@ -11,7 +11,7 @@ import SvgUser from '@/Components/Images/Svgs/Outline/SvgUser.vue'
         :href="route('login')"
         class="w-8 h-8 flex m-auto border focus:border-2 border-gray-900 dark:border-gray-100 hover:border-gray-600 dark:hover:border-gray-300 rounded-full"
       >
-        <SvgUser class="w-4 h-4 lg:m-auto stroke-gray-900 hover:stroke-gray-600 dark:stroke-gray-100 dark:hover:stroke-gray-300" />
+        <OutlineUser class="w-4 h-4 lg:m-auto stroke-gray-900 hover:stroke-gray-600 dark:stroke-gray-100 dark:hover:stroke-gray-300" />
       </a>
     </template>
 
