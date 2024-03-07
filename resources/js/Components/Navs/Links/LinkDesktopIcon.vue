ca5260df3ed1f0d3d7e8b453a7bd2b22a8a10fc5
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-07 18:56:59 +0000

    Created LinkDesktopIcon component

---
+++ resources/js/Components/Navs/Links/LinkDesktopIcon.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+const props = defineProps({
+  href: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+  },
+})
+
+const baseClasses = 'w-8 h-8 flex m-auto border focus:border-2 border-gray-900 dark:border-gray-100 hover:border-gray-600 dark:hover:border-gray-300 rounded-full'
+
+</script>
+
+<template>
+  <Link :href="href" :class="baseClasses">
+    <slot />
+  </Link>
+</template>
