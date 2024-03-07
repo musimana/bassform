a8c9a2c4923663d1f6a4e52a79d19880d788245b
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-07 16:10:31 +0000

    Created AppBlocks component

---
+++ resources/js/Components/Blocks/AppBlocks.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div
+    v-for="(block,index) in $page.props.content.blocks"
+    :key="index"
+    v-html="block.html"
+    class="px-4 pb-12 text-sm text-gray-900 dark:text-gray-100"
+  ></div>
+</template>
