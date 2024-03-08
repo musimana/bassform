43a77630d694cb63b6cbc706c8b65a22a204e1d6
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-08 19:57:58 +0000

    Created Block model

---
+++ app/Models/Block.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Block extends Model
+{
+    use HasFactory,
+        SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'parent_type',
+        'parent_id',
+        'type',
+        'display_order',
+        'data',
+    ];
+
+    /**
+     * Get an array of the block's JSON encoded data field.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function getDataArray(): ?array
+    {
+        return json_decode($this->data ?? '', true);
+    }
+}
