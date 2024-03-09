d3437e6ea0b8556cdc8fbfd2338161f6ca9679ab
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-09 17:22:41 +0000

    Created BlockIndexPanels component

---
+++ resources/js/Components/Blocks/BlockIndexPanels.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AppPanelLink from '@/Components/Sections/AppPanelLink.vue'
+</script>
+
+<template>
+  <section
+    v-if="$page.props.content?.items.length"
+    class="w-full mb-8 px-4 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8"
+  >
+  <AppPanelLink
+      v-for="(item,index) in $page.props.content?.items"
+      :key="index"
+      :item="item"
+    />
+  </section>
+
+  <section
+    v-else
+    class="w-full mb-8 px-4 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8"
+  >
+    <em class="block text-center text-gray-600 dark:text-gray-400">No results found.</em>
+  </section>
+</template>
--- resources/js/Pages/Public/Partials/ContentHomepage.vue
+++ resources/js/Pages/Public/Partials/ContentHomepage.vue
@@ -1,7 +1,7 @@
 <script setup>
-import AppPanelLink from '@/Components/Sections/AppPanelLink.vue'
 import AppSectionDivider from '@/Components/Sections/AppSectionDivider.vue'
 import AppSectionHtml from '@/Components/Sections/AppSectionHtml.vue'
+import BlockIndexPanels from '@/Components/Blocks/BlockIndexPanels.vue'
 </script>
 
 <template>
@@ -13,17 +13,5 @@ import AppSectionHtml from '@/Components/Sections/AppSectionHtml.vue'
     class="px-4"
   />
 
-  <div class="w-full mb-8">
-    <div
-      v-if="$page.props.content?.items.length"
-      class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8"
-    >
-      <AppPanelLink
-        v-for="(item,index) in $page.props.content?.items"
-        :key="index"
-        :item="item"
-      />
-    </div>
-    <em v-else class="block text-center text-gray-600 dark:text-gray-400">No results found.</em>
-  </div>
+  <BlockIndexPanels />
 </template>
