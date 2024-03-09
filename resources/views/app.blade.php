f2a699e1dd716b13e4633a3a20a32630aaa1fe8d
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-09 18:53:07 +0000

    Moved app blade template head element to dedicated template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,34 +1,7 @@
 <!DOCTYPE html>
 
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title inertia>{{ config('app.name') }}</title>
-        <meta name="description" content="{{ config('metadata.description') }}" inertia />
-        <meta name="canonical" content="{{ route('home') }}" inertia />
-
-        <base href="{{ url('/') }}">
-        <meta name="robots" content="index, follow" />
-
-        @if(config('metadata.author'))
-        <meta name="author" content="{{ config('metadata.author') }}" />
-        @endif
-
-        <!-- Icons -->
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
+    @include('partials.head.default')
 
     <body class="font-sans antialiased">
         @inertia
