d31ac336eaeb554477e63c4df29f6c17523ce29b
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-09 14:44:03 +0000

    Moved article tag to app panel

--- resources/js/Components/Sections/AppPanel.vue
+++ resources/js/Components/Sections/AppPanel.vue
@@ -2,10 +2,10 @@
 
 <template>
   <section class="w-full pt-8">
-    <div class="max-w-7xl mx-auto">
+    <article class="max-w-7xl mx-auto">
       <div class="border border-gray-300 dark:border-transparent p-6 overflow-hidden bg-white dark:bg-gray-600/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-gray-100/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none">
         <slot />
       </div>
-    </div>
+    </article>
   </section>
 </template>
