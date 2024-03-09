00c7ed07961fce7dab97cbccb5ae52dd2ce0e5e4
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-09 17:23:02 +0000

    Updated about page seed data

--- storage/app/seeds/pages.php
+++ storage/app/seeds/pages.php
@@ -17,24 +17,21 @@ $seeds = [
         'title' => 'About',
         'subtitle' => config('app.name'),
         'content' => '
-        <section id="welcome" class="p-4">
-            <p class="text-lg tracking-widest border-l-4 border-gray-900 dark:border-gray-300 p-2 bg-gray-200 dark:bg-gray-800"><em>"Suped-up Laravel Breeze with Vue, Inertia & Tailwind"</em></p>
-            <p class="mt-8">
+            <p class="text-lg tracking-widest mb-8 border-l-4 border-gray-900 dark:border-gray-300 p-2 bg-gray-200 dark:bg-gray-800"><em>"Suped-up Laravel Breeze with Vue, Inertia & Tailwind"</em></p>
+            <p class="px-4 pb-4">
                 Bassform is a light-weight template for scaffolding <a href="https://laravel.com/" target="_blank" rel="noopener noreferrer" class="group inline-flex items-center hover:text-gray-900 dark:hover:text-gray-50 focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-100"><em>Laravel</em></a> apps,
                 with Vue for the UI.
                 It is based on and inspired by the VILT SSR flavour of <a href="https://github.com/laravel/breeze" target="_blank" rel="noopener noreferrer" class="group inline-flex items-center hover:text-gray-900 dark:hover:text-gray-50 focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-100"><em>Laravel Breeze</em></a>.
             </p>
-            <p>
+            <p class="px-4 pb-4">
                 The template includes versions of Breeze\'s authentication, profile & dark mode features,
                 plus additional common UI components, SEO enhancements and a test suite with near total code coverage.
             </p>
-            <p>
+            <p class="px-4 pb-4">
                 The template is open source and once a new project has been initiated from it,
                 you\'re free to customise it however you wish.
             </p>
-        </section>
-
-        <hr class="my-8 border-gray-900 dark:border-gray-100" />
+            <hr class="my-8 border-gray-900 dark:border-gray-100" />
         ',
         'in_sitemap' => 1,
         'meta_title' => 'About',
