d1f627eabf1763002d2afe76dff951ace11e63b3
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-15 23:51:07 +0000

    Updated styling of sections components

--- resources/js/Components/Sections/AppHeader.vue
+++ resources/js/Components/Sections/AppHeader.vue
@@ -5,12 +5,12 @@ import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
-  <header class="w-full flex h-18 fixed top-0 lg:right-0 m-auto px-6 lg:px-8 py-6 bg-gray-100 dark:bg-gray-700 dark:bg-gradient-to-bl from-gray-700 via-transparent dark:ring-1 dark:ring-inset dark:ring-gray-100/5 shadow-2xl shadow-gray-500/20 dark:shadow-none">
+  <header class="w-full flex h-18 fixed top-0 lg:right-0 z-50 m-auto px-6 lg:px-8 py-6 bg-gray-100 dark:bg-gray-700 dark:bg-gradient-to-bl from-gray-700 via-transparent text-gray-900 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300 dark:ring-1 dark:ring-inset dark:ring-gray-100/5 shadow-2xl shadow-gray-500/20 dark:shadow-none">
     <div class="w-1/2 flex">
-      <Link id="site-logo" :href="route('home')" class="my-auto shrink-0 flex items-start space-x-4">
-        <IconAppLogo class="w-8 h-8 stroke-gray-900 fill-gray-900 hover:stroke-gray-600 hover:fill-gray-600 dark:stroke-gray-100 dark:fill-gray-100 dark:hover:stroke-gray-300 dark:hover:fill-gray-300" />
+      <Link id="site-logo" :href="route('home')" class="my-auto shrink-0 flex items-start space-x-4 focus:outline-none focus:rounded-sm focus:ring-2 focus:ring-gray-900 dark:focus:ring-gray-100">
+        <IconAppLogo class="w-8 h-8" />
 
-        <h1 id="site-title" class="my-auto text-lg lg:text-xl font-semibold text-gray-900 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300">{{ $page.props.metadata.appName }}</h1>
+        <h1 id="site-title" class="my-auto text-lg lg:text-xl font-semibold">{{ $page.props.metadata.appName }}</h1>
       </Link>
     </div>
 
--- resources/js/Components/Sections/AppMainContent.vue
+++ resources/js/Components/Sections/AppMainContent.vue
@@ -1,7 +1,7 @@
 <script setup />
 
 <template>
-  <main class="w-full min-h-screen mx-auto lg:mt-0 px-6 lg:px-8 py-20 lg:py-28">
+  <main class="w-full min-h-screen mx-auto lg:mt-0 px-6 lg:px-8 py-20 md:py-28">
     <slot />
   </main>
 </template>
