efbf6a8775a4ea51c48d7fde795e82d6f18a576c
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-15 23:02:18 +0000

    Created & applied ControlsSectionToggles page partial component

--- resources/js/Pages/Public/Partials/Controls/ControlsTabButtons.vue
+++ resources/js/Pages/Public/Partials/Controls/ControlsTabButtons.vue
@@ -1,10 +1,13 @@
 <script setup>
 import ControlsSectionButtons from '@/Pages/Public/Partials/Controls/ControlsSectionButtons.vue'
 import ControlsSectionLinks from '@/Pages/Public/Partials/Controls/ControlsSectionLinks.vue'
+import ControlsSectionToggles from '@/Pages/Public/Partials/Controls/ControlsSectionToggles.vue'
 </script>
 
 <template>
   <ControlsSectionButtons />
 
   <ControlsSectionLinks />
+
+  <ControlsSectionToggles />
 </template>
