48baa2346e851074ab449d190f2a90ca4c421024
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-15 22:58:26 +0000

    Created & applied ButtonIcon component

---
+++ resources/js/Components/Controls/Buttons/ButtonIcon.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  customClasses: {
+    type: String,
+    default: 'w-8 h-8 flex m-auto border focus:border-2 border-gray-900 dark:border-yellow-300 hover:border-gray-600 dark:hover:border-yellow-500 rounded-full',
+  },
+  type: {
+    type: String,
+    default: 'button',
+  },
+})
+</script>
+
+<template>
+  <button
+    :class="customClasses"
+    :type="type"
+  >
+    <slot />
+  </button>
+</template>
--- resources/js/Components/Controls/ControlDarkMode.vue
+++ resources/js/Components/Controls/ControlDarkMode.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ButtonIcon from '@/Components/Controls/Buttons/ButtonIcon.vue'
 import OutlineMoon from '@/Components/Icons/HeroIcons/Outline/OutlineMoon.vue'
 import OutlineSun from '@/Components/Icons/HeroIcons/Outline/OutlineSun.vue'
 import { UseDark } from '@vueuse/components'
@@ -6,16 +7,19 @@ import { UseDark } from '@vueuse/components'
 defineProps({
   customClasses: {
     type: String,
-    default: 'w-8 h-8 flex m-auto border focus:border-2 border-gray-900 dark:border-yellow-300 hover:border-gray-600 dark:hover:border-yellow-500 rounded-full',
+    default: 'w-8 h-8 flex border border-gray-900 dark:border-yellow-300 hover:border-gray-600 dark:hover:border-yellow-500 rounded-full text-gray-900 hover:text-gray-600 dark:text-yellow-300 dark:hover:text-yellow-500',
   },
 })
 </script>
 
 <template>
   <UseDark v-slot="{ isDark, toggleDark }">
-    <button :class="customClasses" @click="toggleDark()">
-      <OutlineSun v-if="isDark" class="w-5 h-5 lg:m-auto dark:stroke-yellow-300 dark:hover:stroke-yellow-500" />
-      <OutlineMoon v-else class="w-4 h-4 lg:m-auto stroke-gray-900 hover:stroke-gray-600" />
-    </button>
+    <ButtonIcon
+      :custom-classes="customClasses"
+      @click="toggleDark()"
+    >
+      <OutlineSun v-if="isDark" class="w-5 h-5 m-auto" />
+      <OutlineMoon v-else class="w-4 h-4 m-auto" />
+    </ButtonIcon>
   </UseDark>
 </template>
