359696129a4edbdb2e00f6b03ff1f646ddf63b57
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-15 23:46:32 +0000

    Refactored ControlsSectionButtons component

--- resources/js/Pages/Public/Partials/Controls/ControlsSectionButtons.vue
+++ resources/js/Pages/Public/Partials/Controls/ControlsSectionButtons.vue
@@ -1,26 +1,48 @@
 <script setup>
 import AppButton from '@/Components/Controls/Buttons/AppButton.vue'
+import InputSuccess from '@/Components/Forms/Inputs/InputSuccess.vue'
+import { reactive } from 'vue'
+
+const state = reactive({
+  message: '',
+})
+
 </script>
 
 <template>
-  <section id="template-ui-buttons" class="my-8">
-    <h3 class="w-full pb-4 font-semibold text-xs text-gray-950 dark:text-gray-100 uppercase tracking-widest">Button Components</h3>
+  <section id="template-ui-buttons" class="py-8">
+    <header class="flex">
+      <h3 class="w-full md:w-1/2 my-auto pb-4 font-semibold text-xs text-gray-950 dark:text-gray-100 uppercase tracking-widest">Button Components</h3>
 
-    <div class="w-full p-4 flex justify-between bg-gray-200 dark:bg-gray-700">
-      <AppButton
-        custom-classes="app-button-primary-gray-900 w-48 mx-auto flex justify-center"
-        type="button"
-      >Primary Gray 900</AppButton>
+      <InputSuccess
+        v-show="state.message !== ''"
+        class="w-full md:w-1/2 my-auto pb-3 text-end"
+        :message="state.message"
+      />
+    </header>
 
+    <div class="w-full p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8 justify-between bg-gray-200 dark:bg-gray-700">
       <AppButton
-        custom-classes="app-button-primary-red-600 w-48 mx-auto flex justify-center"
+        v-for="(button,index) in [
+          {
+            label: 'Primary Gray 900',
+            classes: 'app-button-primary-gray-900 mx-auto',
+          },
+          {
+            label: 'Primary Red 600',
+            classes: 'app-button-primary-red-600 mx-auto',
+          },
+          {
+            label: 'Secondary Gray 100',
+            classes: 'app-button-secondary-gray-100 mx-auto',
+          },
+        ]"
+        :key="index"
+        :custom-classes="button.classes"
+        min-width="lg"
         type="button"
-      >Primary Red 600</AppButton>
-
-      <AppButton
-        custom-classes="app-button-secondary-gray-100 w-48 mx-auto flex justify-center"
-        type="button"
-      >Secondary Gray 100</AppButton>
+        @click="state.message = 'Button clicked: ' + button.label"
+      >{{ button.label }}</AppButton>
     </div>
   </section>
 </template>
