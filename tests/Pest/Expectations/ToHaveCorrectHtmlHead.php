87a2f62de44da9e1ff544c495e8335420c71c8a8
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-22 21:41:15 +0000

    Reverted to production mode when compiling the assets for CI tests

--- tests/Pest/Expectations/ToHaveCorrectHtmlHead.php
+++ tests/Pest/Expectations/ToHaveCorrectHtmlHead.php
@@ -43,9 +43,12 @@ expect()->extend('toHaveCorrectHtmlHead', function (string $template) {
             '<script type="text/javascript">',
             'Ziggy',
             '</script>',
-            '<script type="module" src="http://[::1]:5173/@vite/client"></script>',
-            '<script type="module" src="http://[::1]:5173/resources/js/app.js"></script>',
-            '<script type="module" src="http://[::1]:5173/resources/js/Pages/' . $template . '.vue"></script>',
+            '<script type="module" src="http://[::1]:',
+            '/@vite/client"></script>',
+            '<script type="module" src="http://[::1]:',
+            '/resources/js/app.js"></script>',
+            '<script type="module" src="http://[::1]:',
+            '/resources/js/Pages/' . $template . '.vue"></script>',
             '</head>',
         ], false);
 
