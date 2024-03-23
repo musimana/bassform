1dc30f2c4306802c75d7090d95108222c6a5bdf9
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-23 22:11:38 +0000

    Updated InputSelect styling

--- resources/js/Components/Forms/Inputs/InputSelect.vue
+++ resources/js/Components/Forms/Inputs/InputSelect.vue
@@ -30,12 +30,12 @@ defineExpose({ focus: () => input.value.focus() })
 
 <template>
   <select
-    class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 border-gray-900 dark:border-gray-100 focus:ring-gray-900 dark:focus:ring-gray-100 rounded-md shadow-sm dark:shadow-none"
+    class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 border-gray-500 focus:border-gray-100 focus:ring-gray-100 rounded-md shadow-sm dark:shadow-none"
     :value="modelValue"
     @change="$emit('update:modelValue', $event.target.value)"
     ref="input"
   >
-    <option :disabled="!hasNull" value="" :class="{ 'hidden' : !hasNull }"><em>none</em></option>
+    <option :disabled="!hasNull" value="" :class="{ 'hidden' : !hasNull }"></option>
 
     <option
       v-for="(option,index) in options"
