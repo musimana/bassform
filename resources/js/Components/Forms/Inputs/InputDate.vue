693c83adab337ae786ccae082eae62adfa7279be
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-23 23:10:05 +0000

    Created InputDate component

---
+++ resources/js/Components/Forms/Inputs/InputDate.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const model = defineModel({
+  type: String,
+  required: true,
+})
+
+const input = ref(null)
+
+onMounted(() => {
+  if (input.value.hasAttribute('autofocus')) {
+    input.value.focus()
+  }
+})
+
+defineExpose({ focus: () => input.value.focus() })
+</script>
+
+<template>
+  <input
+    class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 border-gray-500 focus:border-gray-100 focus:ring-gray-100 rounded-md shadow-sm dark:shadow-none"
+    v-model="model"
+    ref="input"
+    type="date"
+  />
+</template>
