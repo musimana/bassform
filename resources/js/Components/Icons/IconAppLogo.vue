91cf92f06be6cebfa7f1d0475c34cf26ab32490c
Graham Aitken <graham.b.aitken@gmail.com>
2024-03-25 23:55:04 +0000

    Added stroke attributes to IconAppLogo

--- resources/js/Components/Icons/IconAppLogo.vue
+++ resources/js/Components/Icons/IconAppLogo.vue
@@ -7,8 +7,8 @@
     stroke="currentColor"
     stroke-width="1"
   >
-    <circle cx="512" cy="512" r="496" stroke-width="32" fill="none" />
+    <circle cx="512" cy="512" r="496" stroke-width="32" fill="none" stroke-linecap="round" stroke-linejoin="round" />
     <polygon points="488,128 896,203 536,188 536,248 848,302 536,293 536,674 491,680" />
-    <circle cx="332" cy="680" r="180" stroke-width="48" fill="none" />
+    <circle cx="332" cy="680" r="180" stroke-width="48" fill="none" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
 </template>
