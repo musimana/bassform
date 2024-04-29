cf5f5efc0555eef86222440bf1f8fabcd8f3857d
Graham Aitken <graham.b.aitken@gmail.com>
2024-04-29 21:01:40 +0100

    Create CreateEditPageResource with unit tests

---
+++ app/Http/Resources/Views/Admin/Pages/CreateEditPageResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Views\Admin\Pages;
+
+use App\Http\Resources\Views\Blocks\BlocksResource;
+use App\Interfaces\Resources\Items\PageItemInterface;
+use App\Models\Page;
+
+final class CreateEditPageResource implements PageItemInterface
+{
+    /**
+     * Get the content array for the given page's full public resource.
+     *
+     * @return array<string, array<int, array<int|string, mixed>>|bool|string>
+     */
+    public function getItem(Page $page): array
+    {
+        return [
+            'blocks' => (new BlocksResource)->getItems($page->blocks),
+            'subtitle' => $page->getSubtitle(),
+            'title' => $page->getTitle(),
+            'metaTitle' => $page->getMetaTitle(),
+            'metaDescription' => $page->getMetaDescription(),
+            'inSitemap' => Page::where('id', $page->id)->inSitemap()->exists(),
+        ];
+    }
+}
