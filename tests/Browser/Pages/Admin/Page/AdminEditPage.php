cb72b1f094a7bf2c53f5e866f9bdf4db5847eca0
Graham Aitken <graham.b.aitken@gmail.com>
2024-05-12 22:13:49 +0100

    Create browser test AdminEditPage page class

---
+++ tests/Browser/Pages/Admin/Page/AdminEditPage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Browser\Pages\Admin\Page;
+
+use App\Models\Page;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\AdminPage;
+
+final class AdminEditPage extends AdminPage
+{
+    /** Instantiate the class. */
+    public function __construct(
+        private Page $page
+    ) {
+    }
+
+    /** Get the URL for the page. */
+    public function url(): string
+    {
+        return $this->page->getUrlEdit();
+    }
+
+    /** Assert that the browser is on the page. */
+    public function assert(Browser $browser): void
+    {
+        $browser
+            ->assertHasLoadedCorrectly($this->url())
+            ->assertHasCorrectCookies()
+            ->assertHasRenderedCorrectly($this->page->getTitle())
+
+            ->pause(config('dusk.pause_length'));
+    }
+}
