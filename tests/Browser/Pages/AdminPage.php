2a5250b2510187a58fa6569341f50aaf00ae34af
Graham Aitken <graham.b.aitken@gmail.com>
2024-05-12 22:08:37 +0100

    Create browser test abstract AdminPage

---
+++ tests/Browser/Pages/AdminPage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser\Pages;
+
+use Laravel\Dusk\Browser;
+use Tests\Browser\Traits\HasForm;
+
+abstract class AdminPage extends Page
+{
+    use HasForm;
+
+    /** Assert the page has loaded correctly. */
+    public function assertHasLoadedCorrectly(Browser $browser, string $url): void
+    {
+        $browser
+            ->waitForLocation($url, config('tests.wait_length'))
+            ->assertUrlIs($url)
+            ->assertScript('window.__VUE__')
+            ->assertScript('document.readyState', 'complete');
+    }
+
+    /** Assert the page has rendered correctly. */
+    public function assertHasRenderedCorrectly(Browser $browser, string $title): void
+    {
+        $browser
+            ->assertTitle($title . ' - ' . config('app.name'))
+            ->assertVisible('#app')
+            ->assertVisible('main');
+    }
+}
