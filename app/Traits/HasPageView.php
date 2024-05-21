e4b7861a4e1a6494e1f588b7b1ae61c95dadeb11
Graham Aitken <graham.b.aitken@gmail.com>
2024-05-21 15:14:15 +0100

    Add qualified return type

--- app/Traits/HasPageView.php
+++ app/Traits/HasPageView.php
@@ -31,7 +31,11 @@ trait HasPageView
         return $this->title ?? '';
     }
 
-    /** The relationship for the resource's content blocks. */
+    /**
+     * The relationship for the resource's content blocks.
+     *
+     * @return HasMany<Block>
+     */
     public function blocks(): HasMany
     {
         return $this->hasMany(Block::class, 'parent_id')
