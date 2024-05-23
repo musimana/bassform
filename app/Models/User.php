64cfdb8f1a422df6a580aa1f27bc4199bbfadac9
Graham Aitken <graham.b.aitken@gmail.com>
2024-05-23 12:05:09 +0100

    Uninstall Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 final class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens,
-        HasFactory,
+    use HasFactory,
         HasUuids,
         Notifiable,
         SoftDeletes;
