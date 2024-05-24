58e6d0574b903582ebb781b11221e6eeed86a2e0
Graham Aitken <graham.b.aitken@gmail.com>
2024-05-25 00:12:10 +0100

    Update count call to use collection count method

--- database/factories/BlockFactory.php
+++ database/factories/BlockFactory.php
@@ -21,7 +21,7 @@ final class BlockFactory extends Factory
     {
         $type = fake()->randomElement(Block::typeValues());
         $page = Page::inRandomOrder()->first() ?? Page::factory()->create();
-        $blocks_count = count($page->blocks);
+        $blocks_count = $page->blocks->count();
         $display_order = $blocks_count ? $blocks_count + 1 : 0;
         $data = $type === BlockType::TABS->value ? json_encode(['tabs' => ['Tab One', 'Tab Two']]) : null;
 
