1cc712082813ff2b47e00a2c08a86b8d8a725d97
Graham Aitken <graham.b.aitken@gmail.com>
2024-05-28 19:41:01 +0100

    Create PrivacyPolicyController with route & feature tests

---
+++ tests/Feature/Http/Controllers/Public/PrivacyPolicyControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+use App\Http\Resources\Views\DetailsViewResource;
+use App\Http\Resources\Views\Public\Content\PageContentResource;
+use App\Http\Resources\Views\Public\Metadata\PageMetadataResource;
+use App\Models\Page;
+
+it('can render the privacy page view without a Page model', function () {
+    $page = Page::factory()->privacyPage()->make();
+    $route = route('privacy');
+    $actual = $this->get($route);
+    $session = session()->all();
+    $headers = $actual->headers->all();
+
+    $actual
+        ->assertStatus(200)
+        ->assertSessionHasNoErrors()
+        ->assertViewIs('app');
+
+    expect($session)->toHaveCorrectSessionValues($route);
+
+    expect($headers)->toHaveCorrectHeaderValues();
+
+    $data = (new DetailsViewResource)->getItem(
+        (new PageContentResource)->getItem($page),
+        (new PageMetadataResource)->getItem($page)
+    );
+
+    expect($actual)
+        ->toHaveCorrectHtmlHead($page->template)
+        ->toHaveCorrectHtmlBody()
+        ->toHaveCorrectPropsDetails(
+            $page->template,
+            $data['content'],
+            $data['metadata']
+        );
+});
+
+it('can render the privacy page view with a Page model', function () {
+    $page = Page::factory()->create(['slug' => 'privacy']);
+    $route = route('privacy');
+    $actual = $this->get($route);
+    $session = session()->all();
+    $headers = $actual->headers->all();
+
+    $actual
+        ->assertStatus(200)
+        ->assertSessionHasNoErrors()
+        ->assertViewIs('app');
+
+    expect($session)->toHaveCorrectSessionValues($route);
+
+    expect($headers)->toHaveCorrectHeaderValues();
+
+    $data = (new DetailsViewResource)->getItem(
+        (new PageContentResource)->getItem($page),
+        (new PageMetadataResource)->getItem($page)
+    );
+
+    expect($actual)
+        ->toHaveCorrectHtmlHead($page->template)
+        ->toHaveCorrectHtmlBody()
+        ->toHaveCorrectPropsDetails(
+            $page->template,
+            $data['content'],
+            $data['metadata']
+        );
+});
