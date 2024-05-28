b113a1944f872ffdb187b3c31777504cbbe46bfb
Graham Aitken <graham.b.aitken@gmail.com>
2024-05-28 19:24:22 +0100

    Update AppFooter to use the new components

--- resources/js/Components/Sections/AppFooter.vue
+++ resources/js/Components/Sections/AppFooter.vue
@@ -1,28 +1,14 @@
 <script setup>
-import IconGithub from '@/Components/Icons/IconGithub.vue'
+import FooterCopyright from '@/Components/Sections/Footers/FooterCopyright.vue'
+import FooterSocialLinks from '@/Components/Sections/Footers/FooterSocialLinks.vue'
 </script>
 
 <template>
-  <footer class="absolute bottom-0 flex w-full m-auto pb-4 md:pt-4 px-6 lg:px-8 items-center justify-between">
-    <div class="w-1/2 text-sm text-gray-900 dark:text-gray-100 text-start">
-      <div v-if="$page.props.metadata.copyright !== ''" class="flex items-center gap-2">
-        {{ $page.props.metadata.copyright }}
-      </div>
-    </div>
+  <footer
+    class="absolute bottom-0 w-full flex m-auto pb-4 md:pt-4 px-6 lg:px-8 items-center justify-between text-sm text-gray-900 dark:text-gray-100"
+  >
+    <FooterCopyright />
 
-    <div class="w-1/2 text-sm text-gray-900 dark:text-gray-100 text-end">
-      <nav class="flex justify-end items-center gap-2">
-        <a
-          v-if="$page.props.metadata.links.github"
-          class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 dark:focus:ring-gray-100 focus:rounded-sm"
-          :href="$page.props.metadata.links.github"
-          title="Github Repo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <IconGithub class="w-6 h-6 fill-gray-900 dark:fill-gray-100 hover:fill-gray-600 dark:hover:fill-gray-300 cursor-pointer" />
-        </a>
-      </nav>
-    </div>
+    <FooterSocialLinks />
   </footer>
 </template>
