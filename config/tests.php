ce8e74a9e33069851a019115f3004fdf7db46550
Graham Aitken <graham.b.aitken@gmail.com>
2024-05-28 14:15:39 +0100

    Move Dusk test config into tests config

--- config/tests.php
+++ config/tests.php
@@ -13,4 +13,113 @@ return [
 
     'default_password' => env('TESTS_DEFAULT_PASSWORD', 'password'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Dusk Browser Tests Settings
+    |--------------------------------------------------------------------------
+    |
+    | Setting that control the Dusk browser test suite.
+    |
+    */
+
+    'dusk' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Headless Disabled
+        |--------------------------------------------------------------------------
+        |
+        | Determine whether the Dusk command has disabled headless mode.
+        |
+        | Set the `DUSK_HEADLESS_DISABLED` DotEnv variable to `true` to run the
+        | tests in a browser window, or `false` to run them purely from the command
+        | line.
+        |
+        | This option is referenced by both the app & the Laravel Dusk package.
+        |
+        */
+
+        'headless_disabled' => env('DUSK_HEADLESS_DISABLED', true),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Pause Length (milliseconds)
+        |--------------------------------------------------------------------------
+        |
+        | The length of time in milliseconds to pause for after each browser
+        | wherever the pause method is called by browser tests.
+        |
+        | Increasing this value can be useful for debugging, or tutorial video
+        | generation.
+        |
+        | This option is only referenced by the app.
+        |
+        */
+
+        'pause_length' => env('DUSK_PAUSE_LENGTH', 1000),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Wait Length (seconds)
+        |--------------------------------------------------------------------------
+        |
+        | The length of time in seconds to wait for an action to complete wherever
+        | a the pause method is called by browser tests.
+        |
+        | Increasing this value can be useful for debugging, or tutorial video
+        | generation.
+        |
+        | This option is only referenced by the app.
+        |
+        */
+
+        'wait_length' => env('DUSK_WAIT_LENGTH', 5),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Screen Width
+        |--------------------------------------------------------------------------
+        |
+        | The width in pixels of the browser window to be used during browser
+        | tests.
+        |
+        | This option is only referenced by the app.
+        |
+        */
+
+        'screen_width' => env('DUSK_SCREEN_WIDTH', 1920),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Screen Height
+        |--------------------------------------------------------------------------
+        |
+        | The height in pixels of the browser window to be used during browser
+        | tests.
+        |
+        | This option is only referenced by the app.
+        |
+        */
+
+        'screen_height' => env('DUSK_SCREEN_HEIGHT', 1080),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Start Maximized
+        |--------------------------------------------------------------------------
+        |
+        | Determines if the browser window size should be maximised at the start of
+        | each test. Requires headless mode to be disabled to have any effect.
+        |
+        | Should be set to false for the screen width and height variables to have
+        | any effect.
+        |
+        | This option is referenced by both the app & the Laravel Dusk package.
+        |
+        */
+
+        'start_maximised' => env('DUSK_START_MAXIMISED', false),
+
+    ],
+
 ];
