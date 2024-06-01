adb961b8a07f6e6e44e1538b9de1664a4bb99e0f
Graham Aitken <graham.b.aitken@gmail.com>
2024-06-01 14:42:23 +0100

    Add type method to BlockFactory

--- database/factories/BlockFactory.php
+++ database/factories/BlockFactory.php
@@ -21,16 +21,24 @@ final class BlockFactory extends Factory
     {
         $type = fake()->randomElement(Block::typeValues());
         $page = Page::inRandomOrder()->first() ?? Page::factory()->create();
-        $blocks_count = $page->blocks->count();
-        $display_order = $blocks_count ? $blocks_count + 1 : 0;
-        $data = $type === BlockType::TABS->value ? json_encode(['tabs' => ['Tab One', 'Tab Two']]) : null;
+        $display_order = $page->blocks->count();
 
         return [
             'type' => $type,
             'parent_type' => Page::class,
             'parent_id' => $page->id,
             'display_order' => $display_order,
-            'data' => $data,
+            'data' => null,
         ];
+    }
+
+    /** Indicate the type for the block model. */
+    public function type(BlockType $block_type, ?string $json = null): static
+    {
+        return $this->state(fn (array $attributes) => array_merge([
+            ...$attributes,
+            'type' => $block_type->value,
+            'data' => $json,
+        ]));
     }
 }
