c29a72d1d9a562919f68d90ceafd02962e6b0669
Graham Aitken <graham.b.aitken@gmail.com>
2024-06-02 12:21:05 +0100

    Add cases to Pages dataset

--- tests/Datasets/Pages.php
+++ tests/Datasets/Pages.php
@@ -1,11 +1,43 @@
 <?php
 
+use App\Enums\Blocks\BlockType;
 use App\Models\Page;
 
-dataset('pages', function () {
+$tabs = ['tabs' => ['Tab One', 'Tab Two'], 'tabContents' => ['<p>Tab one content.</p>', '<p>Tab two content.</p>']];
+
+dataset('pages', function () use ($tabs) {
     return [
-        'generic page' => [fn () => Page::factory()->create()],
-        'homepage' => [fn () => Page::factory()->homePage()->create()],
-        'about page' => [fn () => Page::factory()->aboutPage()->create()],
+        'minimum page record' => [fn () => Page::factory()->create()],
+        'full page record' => [fn () => Page::factory()->dummy()->create()],
+        'homepage' => [fn () => Page::factory()->dummy()->homePage()->create()],
+        'about page' => [fn () => Page::factory()->dummy()->aboutPage()->create()],
+        'privacy page' => [fn () => Page::factory()->dummy()->privacyPage()->create()],
+        'full page with tabs block' => [
+            fn () => Page::factory()->dummy()->hasBlocks(1, fn (array $attributes) => [
+                ...$attributes,
+                'parent_type' => Page::class,
+                'type' => BlockType::TABS->value,
+                'data' => json_encode($tabs),
+            ])->create(),
+        ],
+        'full page with stack block' => [
+            fn () => Page::factory()->dummy()->hasBlocks(1, fn (array $attributes) => [
+                ...$attributes,
+                'parent_type' => Page::class,
+                'type' => BlockType::STACK->value,
+            ])->create(),
+        ],
+        'full page with all blocks' => [
+            fn () => Page::factory()->dummy()->hasBlocks(1, fn (array $attributes) => [
+                ...$attributes,
+                'parent_type' => Page::class,
+                'type' => BlockType::TABS->value,
+                'data' => json_encode($tabs),
+            ])->hasBlocks(1, fn (array $attributes) => [
+                ...$attributes,
+                'parent_type' => Page::class,
+                'type' => BlockType::STACK->value,
+            ])->create(),
+        ],
     ];
 });
