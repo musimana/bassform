efd50f27ebc2e6cae35c6d2090c0d889e851b092
Graham Aitken <graham.b.aitken@gmail.com>
2024-06-02 13:37:37 +0100

    Add staticData method to BlockType enum

--- app/Enums/Blocks/BlockType.php
+++ app/Enums/Blocks/BlockType.php
@@ -2,10 +2,32 @@
 
 namespace App\Enums\Blocks;
 
+use App\Http\Resources\Formatters\LaravelVersionFormatterResource;
+use App\Http\Resources\Formatters\PhpVersionFormatterResource;
+
 enum BlockType: string
 {
     /* List of the content blocks available to the application. */
     case STACK = 'stack';
     case TABS = 'tabs';
     case UNKNOWN = 'unknown';
+
+    /**
+     * Get an array of the block type's static data.
+     *
+     * @return array<string, string>
+     */
+    public function staticData(): array
+    {
+        return match ($this) {
+            self::STACK => [
+                'html' => view('partials.static-blocks.stack', [
+                    'laravel_version' => (new LaravelVersionFormatterResource)->getValue(),
+                    'php_version' => (new PhpVersionFormatterResource)->getValue(),
+                ])->render(),
+            ],
+            self::UNKNOWN => ['html' => ''],
+            default => [],
+        };
+    }
 }
