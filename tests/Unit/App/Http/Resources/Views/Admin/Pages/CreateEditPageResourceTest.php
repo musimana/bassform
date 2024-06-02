4bf73a01909324009c797cc243ac414a618215db
Graham Aitken <graham.b.aitken@gmail.com>
2024-06-02 12:23:53 +0100

    Add test using PageGhosts to CreateEditPageResourceTest

--- tests/Unit/App/Http/Resources/Views/Admin/Pages/CreateEditPageResourceTest.php
+++ tests/Unit/App/Http/Resources/Views/Admin/Pages/CreateEditPageResourceTest.php
@@ -29,3 +29,21 @@ test('getItem returns ok with stored models', function (Page $page) {
             'inSitemap' => $page->isInSitemap(),
         ]);
 })->with('pages');
+
+test('getItem returns ok with ghost models', function (Page $page) {
+    $actual = (new CreateEditPageResource)->getItem($page);
+
+    expect($actual)
+        ->toHaveCamelCaseKeys()
+        ->toHaveCount(8)
+        ->toMatchArray([
+            'id' => $page->id,
+            'blocks' => (new BlocksResource)->getItems($page->blocks),
+            'content' => $page->getContent(),
+            'subtitle' => $page->getSubtitle(),
+            'title' => $page->getTitle(),
+            'metaTitle' => $page->getMetaTitle(),
+            'metaDescription' => $page->getMetaDescription(),
+            'inSitemap' => $page->isInSitemap(),
+        ]);
+})->with('page-ghosts');
