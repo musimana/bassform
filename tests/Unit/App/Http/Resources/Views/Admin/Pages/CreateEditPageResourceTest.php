bf1e7519c02465a4af3a2f729394866ebc1efadb
Graham Aitken <graham.b.aitken@gmail.com>
2024-06-02 12:21:39 +0100

    Update CreateEditPageResourceTest to use Pages dataset

--- tests/Unit/App/Http/Resources/Views/Admin/Pages/CreateEditPageResourceTest.php
+++ tests/Unit/App/Http/Resources/Views/Admin/Pages/CreateEditPageResourceTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Resources\Views\Admin\Pages\CreateEditPageResource;
+use App\Http\Resources\Views\Blocks\BlocksResource;
 use App\Interfaces\Resources\Items\PageItemInterface;
 use App\Models\Page;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,17 +12,20 @@ arch('it implements the expected interface')
     ->expect(CreateEditPageResource::class)
     ->toImplement(PageItemInterface::class);
 
-test('getItem returns ok')
-    ->expect(fn () => (new CreateEditPageResource)->getItem(new Page))
-    ->toHaveCamelCaseKeys()
-    ->toHaveCount(8)
-    ->toMatchArray([
-        'id' => null,
-        'blocks' => [],
-        'content' => null,
-        'subtitle' => '',
-        'title' => '',
-        'metaTitle' => '',
-        'metaDescription' => '',
-        'inSitemap' => '',
-    ]);
+test('getItem returns ok with stored models', function (Page $page) {
+    $actual = (new CreateEditPageResource)->getItem($page);
+
+    expect($actual)
+        ->toHaveCamelCaseKeys()
+        ->toHaveCount(8)
+        ->toMatchArray([
+            'id' => $page->id,
+            'blocks' => (new BlocksResource)->getItems($page->blocks),
+            'content' => $page->getContent(),
+            'subtitle' => $page->getSubtitle(),
+            'title' => $page->getTitle(),
+            'metaTitle' => $page->getMetaTitle(),
+            'metaDescription' => $page->getMetaDescription(),
+            'inSitemap' => $page->isInSitemap(),
+        ]);
+})->with('pages');
