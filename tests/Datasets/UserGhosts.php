e62d74869f62d833971189921bbc2b645dec0c80
Graham Aitken <graham.b.aitken@gmail.com>
2024-06-06 10:26:08 +0100

    Add isAdmin user case to user datasets

--- tests/Datasets/UserGhosts.php
+++ tests/Datasets/UserGhosts.php
@@ -3,5 +3,8 @@
 use App\Models\User;
 
 dataset('user-ghosts', function () {
-    return ['generic user' => [fn () => User::factory()->make()]];
+    return [
+        'generic user' => [fn () => User::factory()->make()],
+        'admin user' => [fn () => User::factory()->isAdmin()->make()],
+    ];
 });
--- tests/Datasets/Users.php
+++ tests/Datasets/Users.php
@@ -3,5 +3,8 @@
 use App\Models\User;
 
 dataset('users', function () {
-    return ['generic user' => [fn () => User::factory()->create()]];
+    return [
+        'generic user' => [fn () => User::factory()->create()],
+        'admin user' => [fn () => User::factory()->isAdmin()->create()],
+    ];
 });
