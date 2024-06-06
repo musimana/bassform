2e7a737b9517b4cc8e435e0887a6e4bdaab45f8b
Graham Aitken <graham.b.aitken@gmail.com>
2024-06-06 10:37:12 +0100

    Remove redundant arch tests

--- tests/Unit/App/Interfaces/Resources/Indexes/CollectionIndexInterfaceTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Interfaces\Resources\Indexes\CollectionIndexInterface;
-
-arch('it has a getItems method')
-    ->expect(CollectionIndexInterface::class)
-    ->toHaveMethod('getItems');
--- tests/Unit/App/Interfaces/Resources/items/ConstantItemInterfaceTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Interfaces\Resources\Items\ConstantItemInterface;
-
-arch('it has a getItem method')
-    ->expect(ConstantItemInterface::class)
-    ->toHaveMethod('getItem');
