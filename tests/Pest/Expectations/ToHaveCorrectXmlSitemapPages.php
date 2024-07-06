ba53c5d0d3e435497264d43492f9412124757f84
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-06 18:34:57 +0100

    Split sitemap items template urlset tag across lines for readability

--- tests/Pest/Expectations/ToHaveCorrectXmlSitemapPages.php
+++ tests/Pest/Expectations/ToHaveCorrectXmlSitemapPages.php
@@ -57,7 +57,12 @@ expect()->extend('toHaveCorrectXmlSitemapPages', function ($content) {
     (new TestResponse($this->value))
         ->assertSeeInOrder([
             '<?xml version=\'1.0\' encoding=\'UTF-8\'?>',
-            '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+            '<urlset',
+            'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"',
+            'xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"',
+            'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"',
+            'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"',
+            '>',
             '<url>',
             '<loc>' . url('/') . '</loc>',
             '<lastmod>' . now()->format('Y-m-d') . '</lastmod>',
