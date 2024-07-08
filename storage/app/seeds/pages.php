a21d2d3ae197f5e0f25d2575372b2afba3732552
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-09 00:19:01 +0100

    Add WYSIWYG block to about page seed

--- storage/app/seeds/pages.php
+++ storage/app/seeds/pages.php
@@ -61,24 +61,33 @@ $seeds = [
         'subtitle' => config('app.name'),
         'content' => '
             <p class="text-lg text-gray-800 dark:text-gray-200 tracking-widest mb-8 border-l-4 border-gray-900 dark:border-gray-300 p-2 bg-gray-200 dark:bg-gray-800"><em>"Laravel Breeze variant with Vue, Inertia & Tailwind"</em></p>
-            <p class="px-4 pb-4">
-                Bassform is a light-weight template for scaffolding <a href="https://laravel.com/" target="_blank" rel="noopener noreferrer" class="group inline-flex items-center hover:text-gray-900 dark:hover:text-gray-50 focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-100"><em>Laravel</em></a> apps,
-                with Vue for the UI.
-                It is based on and inspired by the VILT SSR flavour of <a href="https://github.com/laravel/breeze" target="_blank" rel="noopener noreferrer" class="group inline-flex items-center hover:text-gray-900 dark:hover:text-gray-50 focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-100"><em>Laravel Breeze</em></a>.
-            </p>
-            <p class="px-4 pb-4">
-                The template includes versions of Breeze\'s authentication, profile & dark mode features,
-                plus additional common UI components, SEO enhancements and a test suite with near total code coverage.
-            </p>
-            <p class="px-4 pb-4">
-                The template is open source and once a new project has been initiated from it,
-                you\'re free to customise it however you wish.
-            </p>
         ',
         'meta_title' => 'About',
         'meta_description' => config('metadata.description'),
         'template' => WebpageTemplate::PUBLIC_CONTENT->value,
         'blocks' => [
+            [
+                'type' => BlockType::WYSIWYG->value,
+                'data' => json_encode([
+                    'html' => '
+                        <p>
+                            Bassform is a light-weight template for scaffolding
+                            <a href="https://laravel.com/" target="_blank" rel="noopener noreferrer"><i>Laravel</i></a> apps,
+                            with Vue for the UI.
+                            It is based on and inspired by the VILT SSR flavour of
+                            <a href="https://github.com/laravel/breeze" target="_blank" rel="noopener noreferrer"><i>Laravel Breeze</i></a>.
+                        </p>
+                        <p>
+                            The template includes versions of Breeze\'s authentication, profile & dark mode features,
+                            plus additional common UI components, SEO enhancements and a test suite with near total code coverage.
+                        </p>
+                        <p>
+                            The template is open source and once a new project has been initiated from it,
+                            you\'re free to customise it however you wish.
+                        </p>
+                    ',
+                ])
+            ],
             ['type' => BlockType::SECTION_DIVIDER->value],
             ['type' => BlockType::STACK->value],
         ],
