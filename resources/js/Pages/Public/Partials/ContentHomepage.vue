98f677dd48211b69c3093f9fbfdc08301e2e8319
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-09 00:21:28 +0100

    Remove AppSectionDivider & AppSectionHtml from ContentHomepage page partial component

--- resources/js/Pages/Public/Partials/ContentHomepage.vue
+++ resources/js/Pages/Public/Partials/ContentHomepage.vue
@@ -1,18 +1,8 @@
 <script setup>
 import AppBlocks from '@/Components/Blocks/AppBlocks.vue'
-import AppSectionDivider from '@/Components/Sections/AppSectionDivider.vue'
-import AppSectionHtml from '@/Components/Sections/AppSectionHtml.vue'
 </script>
 
 <template>
-  <AppSectionDivider margin="xl" />
-
-  <AppSectionHtml
-    v-if="$page.props.content.bodytext !== ''"
-    :html="$page.props.content.bodytext"
-    class="mb-12 px-4"
-  />
-
   <AppBlocks
     v-if="$page.props.content.blocks ?? []"
   />
