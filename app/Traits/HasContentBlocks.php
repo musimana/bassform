02e1a24b92835c22a9ee60fd492fc74e99806451
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-09 19:18:30 +0100

    Add syncBlocks method to HasContentBlocks trait

--- app/Traits/HasContentBlocks.php
+++ app/Traits/HasContentBlocks.php
@@ -2,7 +2,9 @@
 
 namespace App\Traits;
 
+use App\Http\Resources\Views\Admin\Blocks\AdminBlocksResource;
 use App\Models\Block;
+use App\Services\Admin\BlockAdminService;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait HasContentBlocks
@@ -17,5 +19,26 @@ trait HasContentBlocks
         return $this->hasMany(Block::class, 'parent_id')
             ->where('parent_type', self::class)
             ->orderBy('display_order');
+    }
+
+    /**
+     * Sync the given content blocks array with the model's blocks.
+     *
+     * @param  array<int, array{
+     *  id: int|false,
+     *  type: string,
+     *  data: array<string, array<int, string>|string>,
+     *  schema: array{label: string, inputs: array<int, array<string, bool|int|string>>}
+     * }>|null  $blocks_array
+     */
+    public function syncBlocks(?array $blocks_array): bool
+    {
+        $resource = new AdminBlocksResource(collect(), $this);
+        $resource->setItems($blocks_array ?? []);
+
+        return BlockAdminService::syncBlocks(
+            $resource->getCollection(),
+            $this->blocks
+        );
     }
 }
