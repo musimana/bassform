1b076c7c27e75b311692ae2f0d30ff512196575a
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-09 01:56:24 +0100

    Add setDefaultModel method to HomepageContentResource

--- app/Http/Resources/Views/Public/Content/HomepageContentResource.php
+++ app/Http/Resources/Views/Public/Content/HomepageContentResource.php
@@ -4,8 +4,10 @@ namespace App\Http\Resources\Views\Public\Content;
 
 use App\Http\Resources\Views\Public\Blocks\BlocksResource;
 use App\Http\Resources\Views\Public\Summaries\PageSummaryResource;
+use App\Enums\Blocks\BlockType;
 use App\Enums\Webpages\WebpageTemplate;
 use App\Interfaces\Resources\Items\ConstantItemInterface;
+use App\Models\Block;
 use App\Models\Page;
 
 final class HomepageContentResource implements ConstantItemInterface
@@ -44,5 +46,31 @@ final class HomepageContentResource implements ConstantItemInterface
     public function getTemplate(): ?WebpageTemplate
     {
         return $this->page->getTemplate();
+    }
+
+    /** Set the resource's page to the default homepage model. */
+    public function setDefaultModel(): void
+    {
+        $this->page = new Page([
+            'template' => WebpageTemplate::PUBLIC_INDEX->value,
+            'slug' => 'home',
+            'title' => config('app.name'),
+            'meta_description' => config('metadata.description'),
+            'in_sitemap' => false,
+            'is_homepage' => true,
+        ]);
+
+        $this->page->blocks = collect([
+            new Block([
+                'type' => BlockType::HEADER_LOGO->value,
+                'parent_type' => Page::class,
+                'parent_id' => $this->page->id,
+                'display_order' => 0,
+                'data' => (string) json_encode([
+                    'heading' => config('app.name'),
+                    'subheading' => config('metadata.description'),
+                ]),
+            ]),
+        ]);
     }
 }
