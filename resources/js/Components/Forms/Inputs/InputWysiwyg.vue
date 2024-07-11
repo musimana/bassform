0e585e987a36b4de5d8cedf0c0e2350e069aea98
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-11 13:56:07 +0100

    Update InputWysiwyg component wrapper element & styling

--- resources/js/Components/Forms/Inputs/InputWysiwyg.vue
+++ resources/js/Components/Forms/Inputs/InputWysiwyg.vue
@@ -86,12 +86,12 @@ Editor
 </script>
 
 <template>
-  <div class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 border-gray-500 focus:border-gray-100 focus:ring-gray-100 rounded-md shadow-sm dark:shadow-none">
+  <section class="app-article bg-gray-100 dark:bg-gray-900 border-gray-500 focus:border-gray-100 focus:ring-gray-100 rounded-md shadow-sm dark:shadow-none">
     <CKEditor.component
       :editor="Editor"
       v-model="model"
     ></CKEditor.component>
-  </div>
+  </section>
 </template>
 
 <style>
