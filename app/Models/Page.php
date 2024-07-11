80b010cd0d6a72abecf8fbd62df338898b766437
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-11 17:41:36 +0100

    Remove unused properties from Page model fillable array

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -30,9 +30,6 @@ final class Page extends Model
     protected $fillable = [
         'slug',
         'title',
-        'subtitle',
-        'content',
-        'meta_title',
         'meta_description',
         'meta_keywords',
         'open_graph_id',
