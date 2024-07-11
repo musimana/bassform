dccd3d78c936b5126fb0e19604bf7e0c0c391e1b
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-11 18:38:35 +0100

    Create PrivacyPolicyContentResource with unit tests

---
+++ tests/Unit/App/Http/Resources/Views/Public/Content/PrivacyPolicyContentResourceTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Resources\Views\Public\Content\PrivacyPolicyContentResource;
+use App\Interfaces\Resources\Items\ConstantItemInterface;
+use App\Models\Page;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+arch('it implements the expected interface')
+    ->expect(PrivacyPolicyContentResource::class)
+    ->toImplement(ConstantItemInterface::class);
+
+test('getItem returns ok without a Page Model', function () {
+    $actual = (new PrivacyPolicyContentResource)->getItem();
+
+    expect($actual['blocks'])
+        ->toBeArray()
+        ->toHaveCount(2);
+
+    expect($actual)
+        ->toHaveCamelCaseKeys()
+        ->toHaveCount(4);
+});
+
+test('getItem returns ok with a Page Model', function () {
+    $page = Page::factory()->create([
+        'title' => 'Privacy Policy',
+        'slug' => 'privacy',
+    ]);
+
+    $actual = (new PrivacyPolicyContentResource($page))->getItem();
+
+    expect($actual['blocks'])
+        ->toBeArray()
+        ->toBeEmpty();
+
+    expect($actual)
+        ->toHaveCamelCaseKeys()
+        ->toHaveCount(4)
+        ->toMatchArray([
+            'blocks' => [],
+            'bodytext' => $page->content,
+            'heading' => $page->title,
+            'subheading' => $page->subtitle,
+        ]);
+});
