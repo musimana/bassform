2d10dfa64a16376368e541dbf40db7c8c7a7ad65
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-13 23:11:02 +0100

    Create privacy policy intro static-block template

---
+++ resources/views/partials/static-blocks/privacy/intro.blade.php
@@ -0,0 +1,9 @@
+<p class="text-small-print">
+    This privacy policy (“policy”) will help you understand how {{ config('app.name') }} (“our”, “us”, “we”)
+    uses and protects the data you provide to us when you visit and use
+    <a href="{{ route('home') }}"><u>{{ substr(route('home'), strpos(route('home'), '://') + 3) }}</u></a>
+    (“app”, “website”).
+    The policy also applies when you access our products or services, and applies when you correspond with us in person,
+    by letter, by phone, email or any other means.
+    We keep this policy under regular review and publish any updates on this web page.
+</p>
