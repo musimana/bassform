7aabe326ca7e2e3ffa4f2e8cd171dd13a087bb45
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-13 23:55:23 +0100

    Create privacy policy data-we-may-collect static-block template

---
+++ resources/views/partials/static-blocks/privacy/data-we-may-collect.blade.php
@@ -0,0 +1,44 @@
+<h3 class="text-small-print">
+    DATA WE MAY COLLECT
+</h3>
+
+<p class="text-small-print">
+    We may collect, use, store and/or transfer data depending on the product or service you are accessing,
+    the different kinds of personal data are grouped together as follows:
+</p>
+
+<ul class="text-small-print">
+    <li>
+        <strong>Identity Data</strong> – which includes your name, username or similar identifier, marital status, title, date of birth and gender.
+    </li>
+
+    <li>
+        <strong>Profile Data</strong> - which includes your username and password for any accounts set up to access our services or provide feedback responses.
+    </li>
+
+    <li>
+        <strong>Contact Data</strong> – which includes telephone numbers, and physical and digital addresses.
+    </li>
+
+    <li>
+        <strong>Usage Data</strong> – which includes information about how you use our site, products and services.
+    </li>
+
+    <li>
+        <strong>Marketing and Communications Data</strong> – which includes your preferences in receiving marketing from us and our third parties
+        and your communication preferences.
+    </li>
+
+    <li>
+        <strong>Technical Data</strong> – includes internet protocol (IP) address, your login data, browser type and version, time zone setting and location,
+        browser plug-in types and versions, operating system and platform and other technology on the devices you use to access the site.
+    </li>
+
+    <li>
+        <strong>Financial Data</strong> – which includes your bank account and payment card details.
+    </li>
+
+    <li>
+        <strong>Transaction Data</strong> – includes details about payments to and from you and other details of products and services you have purchased from us.
+    </li>
+</ul>
