0f53255a77b8bd7ee05e46b546b825aa1831d478
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-14 00:27:41 +0100

    Create privacy policy marketing static-block template

---
+++ resources/views/partials/static-blocks/privacy/marketing.blade.php
@@ -0,0 +1,13 @@
+<h3 class="text-small-print">
+    MARKETING
+</h3>
+
+<p class="text-small-print">
+    We would like to send you information about products and services we think you might like.
+</p>
+
+<p class="text-small-print">
+    If you have agreed to receive marketing, you may always opt out at a later date.
+    You can ask us to stop sending you marketing messages at any time by contacting us
+    using the website's contact form or by any other means.
+</p>
