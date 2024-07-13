ec4328e74c6e35bd9ad5b719a6679996e19dafa4
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-14 00:10:09 +0100

    Create privacy policy how-we-collect-your-data static-block template

---
+++ resources/views/partials/static-blocks/privacy/how-we-collect-your-data.blade.php
@@ -0,0 +1,21 @@
+<h3 class="text-small-print">
+    HOW WE COLLECT YOUR DATA
+</h3>
+
+<p class="text-small-print">You directly provide us with most of the data we collect. We collect data and process data when you:</p>
+
+<ul class="text-small-print">
+    <li>Interact with the website.</li>
+    <li>Make a request for our products/services.</li>
+    <li>Requesting marketing communication to be sent to you or others you are acting on behalf of.</li>
+    <li>Voluntarily complete a customer survey or provide feedback on any of our message boards or via email.</li>
+    <li>Correspond with us online or by any other method.</li>
+</ul>
+
+<p class="text-small-print">We may also receive your data indirectly from the following sources:</p>
+
+<ul class="text-small-print">
+    <li>Third parties or publicly available sources – personal data about you may be received through various third parties and public sources
+        such as analytics providers, advertising networks and/or search information providers based inside the European Economic Area (EEA).</li>
+    <li>Contact, Financial and Transaction Data (if applicable) from providers of technical, payment and delivery services inside the EEA.</li>
+</ul>
