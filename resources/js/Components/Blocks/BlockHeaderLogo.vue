3e0ec4aae48486b7c4e9c58a69b501b3392da38e
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-14 13:22:55 +0100

    Use current color for IconAppLogo fill & stroke on BlockHeaderLogo component

--- resources/js/Components/Blocks/BlockHeaderLogo.vue
+++ resources/js/Components/Blocks/BlockHeaderLogo.vue
@@ -11,7 +11,7 @@ defineProps({
 
 <template>
   <div class="flex justify-center gap-x-4 mt-4">
-    <IconAppLogo class="w-12 h-12 stroke-gray-900 fill-gray-900  dark:stroke-gray-100 dark:fill-gray-100" />
+    <IconAppLogo class="w-12 h-12 stroke-current fill-current" />
 
     <h2 class="my-auto text-center text-xl font-bold">{{ blockData.heading }}</h2>
   </div>
