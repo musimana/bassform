40f06b30a8ba1697829a3654e06e6d0a5c7de941
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-14 19:01:04 +0100

    Update CreateEditPage to use tab components

--- resources/js/Pages/Admin/Partials/CreateEditPage.vue
+++ resources/js/Pages/Admin/Partials/CreateEditPage.vue
@@ -4,6 +4,9 @@ import FormInput from '@/Components/Forms/FormInput.vue'
 import InputBlocks from '@/Components/Forms/Inputs/InputBlocks.vue'
 import InputCheckbox from '@/Components/Forms/Inputs/InputCheckbox.vue'
 import InputText from '@/Components/Forms/Inputs/InputText.vue'
+import AppSectionDivider from '@/Components/Sections/AppSectionDivider.vue'
+import TabBody from '@/Components/Controls/Tabs/TabBody.vue'
+import TabControls from '@/Components/Controls/Tabs/TabControls.vue'
 import CreateEditControls from '@/Pages/Admin/Partials/CreateEditControls.vue'
 import { useForm, usePage } from '@inertiajs/vue3'
 
@@ -27,61 +30,78 @@ const form = useForm({
     :endpoint="route('admin.page.edit', page.id)"
     :form="form"
   >
-    <div class="w-full flex pt-4 px-4">
-      <FormInput
-        input-label="Title*"
-        input-label-position="left"
-        input-field="title"
-        :parent-form="form"
-      >
-        <InputText
-          id="input-title"
-          name="title"
-          type="text"
-          class="block w-full"
-          v-model="form.title"
-          autofocus
-        />
-      </FormInput>
-    </div>
+    <TabControls
+      class="pt-4 px-4"
+      :tabs="['Content', 'Meta Data']"
+    />
 
-    <div class="w-full flex pt-4 px-4">
-      <FormInput
-        input-label="Blocks"
-        input-label-position="left"
-        input-field="blocks"
-        :parent-form="form"
-      >
-        <InputBlocks
-          :blocks="form.blocks"
-        />
-      </FormInput>
-    </div>
+    <AppSectionDivider margin="min" />
 
-    <div class="w-full flex pt-4 px-4">
-      <FormInput
-        input-label="Meta Description"
-        input-label-position="left"
-        input-field="metaDescription"
-        :parent-form="form"
-      >
-        <InputText
-          id="input-meta-description"
-          name="metaDescription"
-          type="text"
-          class="block w-full"
-          v-model="form.metaDescription"
-        />
-      </FormInput>
-    </div>
+    <TabBody
+      class="mx-4 pt-4 px-4 bg-white dark:bg-gray-900 rounded-b-md"
+      :tab="0"
+    >
+      <div class="w-full flex pt-4 px-4">
+        <FormInput
+          input-label="Blocks"
+          input-label-position="hidden"
+          input-field="blocks"
+          :parent-form="form"
+        >
+          <InputBlocks
+            :blocks="form.blocks"
+          />
+        </FormInput>
+      </div>
+    </TabBody>
 
-    <div class="w-full flex p-4">
-      <label class="flex items-start">
-        <InputCheckbox class="my-auto" name="inSitemap" v-model:checked="form.inSitemap" />
+    <TabBody
+      class="mx-4 px-4 py-8 bg-white dark:bg-gray-900 rounded-b-md"
+      :tab="1"
+    >
+      <div class="w-full flex pt-2 px-4">
+        <FormInput
+          input-label="Title*"
+          input-label-position="left"
+          input-field="title"
+          :parent-form="form"
+        >
+          <InputText
+            id="input-title"
+            name="title"
+            type="text"
+            class="block w-full"
+            v-model="form.title"
+            autofocus
+          />
+        </FormInput>
+      </div>
 
-        <span class="ms-2 text-gray-700 dark:text-gray-300">In Sitemap?</span>
-      </label>
-    </div>
+      <div class="w-full flex pt-2 px-4">
+        <FormInput
+          input-label="Meta Description"
+          input-label-position="left"
+          input-field="metaDescription"
+          :parent-form="form"
+        >
+          <InputText
+            id="input-meta-description"
+            name="metaDescription"
+            type="text"
+            class="block w-full"
+            v-model="form.metaDescription"
+          />
+        </FormInput>
+      </div>
+
+      <div class="w-full flex pt-2 px-4">
+        <label class="flex items-start text-sm">
+          <InputCheckbox class="my-auto" name="inSitemap" v-model:checked="form.inSitemap" />
+
+          <span class="ms-2 text-gray-700 dark:text-gray-300">In Sitemap?</span>
+        </label>
+      </div>
+    </TabBody>
 
     <CreateEditControls
       :parent-form="form"
