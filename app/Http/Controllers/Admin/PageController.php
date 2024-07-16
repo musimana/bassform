fb567079672e6bba11c37a7399b86202275b3213
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-16 02:13:00 +0100

    Add show method to admin PageController with feature tests

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -6,9 +6,11 @@ use App\Enums\Webpages\WebpageTemplate;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\PageUpdateRequest;
 use App\Http\Resources\Views\Admin\Pages\CreateEditPageResource;
+use App\Http\Resources\Views\Public\Content\PageContentResource;
 use App\Http\Resources\Views\Public\Metadata\PageMetadataResource;
 use App\Models\Page;
 use App\Repositories\Views\AdminViewRepository;
+use App\Repositories\Views\PublicViewRepository;
 use App\Services\Admin\PageAdminService;
 use Illuminate\Http\RedirectResponse;
 use Inertia\Response;
@@ -16,6 +18,25 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 final class PageController extends Controller
 {
+    /**
+     * Display the given page's preview.
+     *
+     * @throws HttpException
+     */
+    public function show(Page $page): RedirectResponse|Response
+    {
+        if (!$page->template) {
+            abort(404);
+        }
+
+        return (new PublicViewRepository)
+            ->getViewDetails(
+                $page->template,
+                (new PageContentResource($page))->getItem(),
+                (new PageMetadataResource)->getItem($page)
+            );
+    }
+
     /**
      * Display the edit view for the given page.
      *
--- tests/Feature/Http/Controllers/Admin/PageControllerTest.php
+++ tests/Feature/Http/Controllers/Admin/PageControllerTest.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\PageUpdateRequest;
 use App\Http\Resources\Views\Admin\Pages\CreateEditPageResource;
 use App\Http\Resources\Views\DetailsViewResource;
+use App\Http\Resources\Views\Public\Content\PageContentResource;
 use App\Http\Resources\Views\Public\Metadata\PageMetadataResource;
 use App\Models\Page;
 use App\Models\User;
@@ -13,6 +14,50 @@ use App\Models\User;
 arch('it extends the expected abstract controller')
     ->expect(PageController::class)
     ->toExtend(Controller::class);
+
+test('show renders the page preview view', function (Page $page) {
+    $route = route('admin.page.show', [$page]);
+    $user = User::factory()->isAdmin()->create();
+    $actual = $this->actingAs($user)->get($route);
+    $session = session()->all();
+    $headers = $actual->headers->all();
+
+    $actual
+        ->assertStatus(200)
+        ->assertSessionHasNoErrors()
+        ->assertViewIs('app');
+
+    expect($session)->toHaveCorrectSessionValues($route);
+
+    expect($headers)->toHaveCorrectHeaderValues();
+
+    $data = (new DetailsViewResource)->getItem(
+        (new PageContentResource($page))->getItem(),
+        (new PageMetadataResource)->getItem($page)
+    );
+
+    expect($actual)
+        ->toHaveCorrectHtmlHead($page->template)
+        ->toHaveCorrectHtmlBody()
+        ->toHaveCorrectPropsDetails(
+            $page->template,
+            $data['content'],
+            $data['metadata']
+        );
+})->with('pages');
+
+test('show renders the 404 view for unknown pages', function () {
+    $url = url('admin/pages/101/preview');
+    $user = User::factory()->isAdmin()->create();
+    $actual = $this->actingAs($user)->get($url);
+    $session = session()->all();
+
+    $actual
+        ->assertStatus(404)
+        ->assertSessionHasNoErrors();
+
+    expect($session)->toHaveCorrectSessionValues($url);
+});
 
 test('edit renders the page create/edit view', function (Page $page) {
     $route = route('admin.page.edit', [$page]);
