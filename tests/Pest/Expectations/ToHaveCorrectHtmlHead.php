0b4c9f4758016b6aac807e3c365c6d571ae4ef8a
Graham Aitken <graham.b.aitken@gmail.com>
2024-07-23 23:28:22 +0100

    Remove font-family check from ToHaveCorrectHtmlHead assertions

--- tests/Pest/Expectations/ToHaveCorrectHtmlHead.php
+++ tests/Pest/Expectations/ToHaveCorrectHtmlHead.php
@@ -38,7 +38,6 @@ expect()->extend('toHaveCorrectHtmlHead', function (string $template) {
             '<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />',
             '<!-- Fonts -->',
             '<link rel="preconnect" href="https://fonts.bunny.net">',
-            '<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />',
             '<!-- Scripts -->',
             '<script type="text/javascript">',
             'Ziggy',
